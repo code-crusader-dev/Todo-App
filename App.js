b2e97df4e635abfd7a38f5eae7ce15a9c42d8343
code-crusader-dev <sepdevansh@gmail.com>
2026-01-08 21:23:29 +0530

    removed errors

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from "./navigation/AppNavigation";
 
 export default function App() {
   return <AppNavigator />;
--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,8 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from '../screens/HomeScreen';
-import AddTaskScreen from '../screens/AddTaskScreen';
+import HomeScreen from "../screens/HomeScreen";
+import AddTaskScreen from "../screens/AddTaskScreen";
 
 const Stack = createNativeStackNavigator();
 
